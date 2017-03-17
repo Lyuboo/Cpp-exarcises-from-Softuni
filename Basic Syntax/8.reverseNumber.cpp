b88845309dfdc7b533ca6047c315fc9fbfe6c115
Lyubomir Apostolov <lyubomir.apostolov@gmail.com>
2017-03-17 09:36:16 +0200

    updated ex8

--- Basic Syntax/8.reverseNumber.cpp
+++ Basic Syntax/8.reverseNumber.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 
 using namespace std;
-
+int reverseFunction(int number)
+{
+    int remainder;
+    while(number != 0)
+    {
+        remainder = number % 10;
+        number = number / 10;
+        cout << remainder;
+    }
+}
 int main()
 {
-    int inputNum, remainder = 1, dividend;
+    int inputNum;
     cout << "Please enter a number: ";
     cin >> inputNum;
 
-    while(inputNum != 0)
-    {
-        remainder = inputNum % 10;
-        inputNum = inputNum / 10;
-        cout << remainder;
-    }
+    reverseFunction(inputNum);
     return 0;
 }
